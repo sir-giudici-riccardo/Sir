e6ee8576a870240afa6a1dd4a021e3f7215fe2e5
sir-giudici-riccardo <89529857+sir-giudici-riccardo@users.noreply.github.com>
2026-09-25 09:18:36 +0200

    test: include Stats helper in JVM regression

--- tests/run_jvm_tests.sh
+++ tests/run_jvm_tests.sh
@@ -6,6 +6,7 @@ rm -rf "$OUT" && mkdir -p "$OUT"
 javac --release 17 -d "$OUT" \
   "$ROOT/source/app/src/main/java/org/sigma/mobileprobe/CpuParsers.java" \
   "$ROOT/source/app/src/main/java/org/sigma/mobileprobe/BenchmarkEngine.java" \
+  "$ROOT/source/app/src/main/java/org/sigma/mobileprobe/Stats.java" \
   "$ROOT/tests/PureJvmTests.java"
 java -cp "$OUT" PureJvmTests
 rm -rf "$OUT"
